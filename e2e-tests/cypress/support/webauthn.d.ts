e6c04b29ca3102e143f1ffcfac06b94772775ebc
Maxwell Gerber <max@stytch.com>
2023-09-28 17:24:44 -0700

    moar types

--- e2e-tests/cypress/support/webauthn.d.ts
+++ e2e-tests/cypress/support/webauthn.d.ts
@@ -1,29 +1,45 @@
 /// <reference types="cypress" />
 
+// CDP WebAuthn Type definitions are taken from https://chromedevtools.github.io/devtools-protocol/tot/WebAuthn
+
 export type AuthenticatorId = string;
 export type AuthenticatorProtocol = 'u2f' | 'ctap2';
 export type AuthenticatorTransport = 'usb' | 'nfc' | 'ble' | 'cable' | 'internal';
 export type Credential = {
   credentialId: string;
   isResidentCredential: boolean;
+  // Relying Party ID the credential is scoped to. Must be set when adding a credential.
   rpId?: string;
+  // The ECDSA P-256 private key in PKCS#8 format. (Encoded as a base64 string when passed over JSON)
   privateKey: string;
+  // An opaque byte sequence with a maximum size of 64 bytes mapping the credential to a specific user. (Encoded as a base64 string when passed over JSON)
   userHandle?: string;
+  // Signature counter. This is incremented by one for each successful assertion. See https://w3c.github.io/webauthn/#signature-counter
   signCount: number;
+  // The large blob associated with the credential. See https://w3c.github.io/webauthn/#sctn-large-blob-extension (Encoded as a base64 string when passed over JSON)
   largeBlob?: string;
 }
 export type Ctap2Version = 'ctap2_0' | 'ctap2_1';
 export type VirtualAuthenticatorOptions = {
   protocol: AuthenticatorProtocol;
+  // Defaults to ctap2_0. Ignored if |protocol| == u2f.
   ctap2Version?: Ctap2Version;
   transport: AuthenticatorTransport;
+  // Defaults to false.
   hasResidentKey?: boolean;
+  // Defaults to false.
   hasUserVerification?: boolean;
+  // If set to true, the authenticator will support the largeBlob extension. https://w3c.github.io/webauthn#largeBlob Defaults to false.
   hasLargeBlob?: boolean;
+  // If set to true, the authenticator will support the credBlob extension. https://fidoalliance.org/specs/fido-v2.1-rd-20201208/fido-client-to-authenticator-protocol-v2.1-rd-20201208.html#sctn-credBlob-extension Defaults to false.
   hasCredBlob?: boolean;
+  // If set to true, the authenticator will support the minPinLength extension. https://fidoalliance.org/specs/fido-v2.1-ps-20210615/fido-client-to-authenticator-protocol-v2.1-ps-20210615.html#sctn-minpinlength-extension Defaults to false.
   hasMinPinLength?: boolean;
+  // If set to true, the authenticator will support the prf extension. https://w3c.github.io/webauthn/#prf-extension Defaults to false.
   hasPrf?: boolean;
+  // If set to true, tests of user presence will succeed immediately. Otherwise, they will not be resolved. Defaults to true.
   automaticPresenceSimulation?: boolean;
+  // Sets whether User Verification succeeds or fails for an authenticator. Defaults to false.
   isUserVerified?: boolean;
 }
 
@@ -48,6 +64,11 @@ export type ClearCredentialsResult = {}
 export type DisableResult = {}
 
 export type EnableParams = {
+  // Whether to enable the WebAuthn user interface.
+  // Enabling the UI is recommended for debugging and demo purposes, as it is closer to the real experience.
+  // Disabling the UI is recommended for automated testing.
+  // Supported at the embedder's discretion if UI is available.
+  // Defaults to false.
   enableUI?: boolean;
 }
 export type EnableResult = {}
@@ -88,8 +109,11 @@ export type SetAutomaticPresenceSimulationResult = {}
 
 export type SetResponseOverrideBitsParams = {
   authenticatorId: AuthenticatorId;
+  // If isBogusSignature is set, overrides the signature in the authenticator response to be zero. Defaults to false.
   isBogusSignature?: boolean;
+  // If isBadUV is set, overrides the UV bit in the flags in the authenticator response to be zero. Defaults to false.
   isBadUV?: boolean;
+  // If isBadUP is set, overrides the UP bit in the flags in the authenticator response to be zero. Defaults to false.
   isBadUP?: boolean;
 }
 export type SetResponseOverrideBitsResult = {}
